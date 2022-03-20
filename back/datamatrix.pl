328c18352c2d002d5b5eb330da7197d8511e30cd
bernstein <bernsteining@protonmail.com>
2022-03-20 21:47:48 +0100

    multiple flavors in CSS

---
+++ back/datamatrix.pl
@@ -0,0 +1,8 @@
+use HTML::Barcode::DataMatrix;
+
+# small perl script to generate pure html/css datamatrixes
+# source at https://github.com/mstratman/HTML-Barcode-DataMatrix 
+# decode with http://boy.co.ua/decode.php
+
+my $barcode = HTML::Barcode::DataMatrix->new(text => 'https://github.com/bernsteining/mandelbulb');
+print $barcode->render;
